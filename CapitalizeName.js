24ea3122cc25e124b62dbaabae9734e2898eda40
Maria M Lamardo <marialamardo@outlook.com>
2018-08-06 21:39:44 -0400

    Update CapitalizeName.js

--- CapitalizeName.js
+++ CapitalizeName.js
@@ -10,3 +10,16 @@ nameChanger = (name) => {
     return fullName
 }
 console.log(nameChanger(name));
+
+
+// ORRR
+
+// Capt = (name) => {
+//   name = name.toLowerCase().split(" ");
+//   first_name = name[0][0].toUpperCase() + name[0].substr(1);
+//   last_name = name[1][0].toUpperCase()+ name[1].substr(1);
+//   return name = first_name + " " + last_name;
+// }
+
+// const name = "pooPy ButtHOle";
+// console.log(Capt(name));
